61eeca7c2f538f3e25ae18163ae5e3d4ae4d3a24
VagabondOfHell <arrow_m13@hotmail.com>
2016-04-07 19:21:23 -0400

    Comment Update
    Added more description as to what's going on + additional comments

--- Equations/Equations/Equations.cpp
+++ Equations/Equations/Equations.cpp
@@ -9,17 +9,28 @@
 int _tmain(int argc, _TCHAR* argv[])
 {
 	Equation eq = Equation();
-
+	//Set a sample equation. We can specify the name of the variables inside the declaration with double quotes
+	//(backslashes for escape sequence)
 	eq.SetEquation("'Vf'\"Final Velocity\" = 'Vi't\"Time\" + 0.5at^2");
-
+	
+	//Set the variables values
 	eq.SetVariable("Vi", 15.0);
 	eq.SetVariable("t", 0.0016);
 	eq.SetVariable("a", 4.0);
 
 	try
 	{
+		std::cout << "The Equation : 'Vf'\"Final Velocity\" = 'Vi't\"Time\" + 0.5at^2 after formatting:" << std::endl;
+		//Display the equation after formatting
 		std::cout << eq.GetFormattedEquation() << std::endl;
-		std::cout << eq.GetAnswer("Vf") << std::endl;
+		
+		std::cout << "\nCalculating Answer for Vf (Final Velocity) when\n" <<
+			"Vi(Initial Velocity) = 15 meters per second\n" <<
+			"t(Time) = 0.0016 seconds\n" <<
+			"and a(Acceleration) = 4.0 meters per second squared\n" << std::endl;
+		
+		//Display the calculated result of Final Velocity	
+		std::cout << "Answer: " << eq.GetAnswer("Vf") << std::endl;
 	}
 	catch (const std::exception& e)
 	{
